4246fe4cc09af7681c1b16840b98f90fbf459545
shahjarar <160387866+shahjarar@users.noreply.github.com>
2024-02-29 17:56:22 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+// Task 20
+// Think of somethinkg you could store in a array. for example, you could make a list.
+// of mountains, rivers countries , cities ,languages, or anythink else you'd like.
+// Write a program that creates a list containing these items.
+var country = ["Pakistan", "india", "New York", "Japan", "china"];
+console.log("List of country:");
+console.log(country);
---
+++ main.ts
@@ -0,0 +1,8 @@
+// Task 20
+// Think of somethinkg you could store in a array. for example, you could make a list.
+// of mountains, rivers countries , cities ,languages, or anythink else you'd like.
+// Write a program that creates a list containing these items.
+
+let country: string [] = ["Pakistan" ,"india" ,"New York" ,"Japan","china"];
+console.log("List of country:");
+console.log(country);
